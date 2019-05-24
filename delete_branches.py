da54fe07cf3836ea87e97f8cd44260faa53e7b39
Maciej Kasprzak <maciej.kasprzak@sofomo.com>
2019-05-24 10:04:23 +0200

    fixed local branch delete tool

--- delete_branches.py
+++ delete_branches.py
@@ -2,7 +2,8 @@ from subprocess import check_output, call
 from re import split
 
 out = check_output(["git", "branch", "--merged"])
-out = split('\n\s*', out)
+print(out)
+out = split('\n\s*', out.decode('UTF-8'))
 out = map(lambda s: s.strip(), out)
 out = filter(lambda s: '*' not in s and 'master' not in s and '' != s, out)
 for branch in out:
